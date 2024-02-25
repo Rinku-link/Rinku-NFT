f18e30234e08f68a6cae19929626fba88c0230c0
Jaylon <jaylon@jaylon.com>
2024-02-25 14:29:26 +0800

    update latest version of nft

--- contracts/GenesisBoomerNFT.sol
+++ contracts/GenesisBoomerNFT.sol
@@ -1,19 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-contract GenesisBoomerNFT is ERC721Enumerable, ReentrancyGuard, Ownable {
+contract GenesisBoomerNFT is ERC721, ReentrancyGuard, Ownable {
     uint256 public constant MAX_SUPPLY = 21000;
-    bool public mintingEnabled = false;
+    string public constant TOKEN_URI = "ipfs://QmRUcmYJHh3soXsi6nAb3vripQBgHmYdsqFUHkzKX8Fsjd";
+    bool public mintingEnabled;
     uint256 public mintPrice;
     uint256 public mintCap;
     uint256 public mintedSum;
+    uint256 public totalSupply;
+    bytes32 public root; // root of merkle tree for airdrops
 
     // Mapping to keep track of addresses that have minted
     mapping(address => bool) public hasMinted;
+    mapping(address => bool) public hasClaimed;
+    mapping(address => bool) public airdropClaimed;
 
     // Events
     event MintPriceChanged(uint256 newPrice);
@@ -22,20 +28,36 @@ contract GenesisBoomerNFT is ERC721Enumerable, ReentrancyGuard, Ownable {
     event Minted(address to, uint256 tokenId);
     event GenesisBoomerProof(address indexed user, string message);
 
-    constructor(uint256 _mintPrice) ERC721("GenesisBoomerNFT", "GBT") {
+    constructor(uint256 _mintPrice) ERC721("GenesisBoomerNFT", "GBT") Ownable(msg.sender) {
         mintPrice = _mintPrice;
+    }
 
-        // Pre-mint 15% of maxSupply
-        uint256 preMintAmount = MAX_SUPPLY * 15 / 100;
-        for (uint256 i = 0; i < preMintAmount; i++) {
-            _safeMint(owner(), totalSupply() + 1);
-            emit Minted(owner(), totalSupply());
+    function reserve(uint256 mintAmount) public onlyOwner {
+        require(totalSupply + mintAmount <= MAX_SUPPLY, "Exceeds max supply");
+
+        for (uint256 i = 1; i <= mintAmount; i++) {
+            totalSupply++;
+            _safeMint(owner(), totalSupply);
         }
+        emit Minted(owner(), mintAmount);
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        _requireOwned(tokenId);
+        return _baseURI();
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return TOKEN_URI;
     }
 
     function setMintPrice(uint256 _newPrice) public onlyOwner {
         mintPrice = _newPrice;
         emit MintPriceChanged(_newPrice);
+    }
+
+    function setMerkleRoot(bytes32 newMerkleRoot) public onlyOwner {
+        root = newMerkleRoot;
     }
 
     function enableMinting(uint256 _mintCap) public onlyOwner {
@@ -53,16 +75,29 @@ contract GenesisBoomerNFT is ERC721Enumerable, ReentrancyGuard, Ownable {
     function mint() public payable nonReentrant {
         require(mintingEnabled, "Minting is not enabled");
         require(mintedSum < mintCap, "Exceeds mint cap");
-        require(totalSupply() + 1 <= MAX_SUPPLY, "Exceeds max supply");
+        require(totalSupply + 1 <= MAX_SUPPLY, "Exceeds max supply");
         require(msg.value >= mintPrice, "Incorrect Ether value");
         require(!hasMinted[msg.sender], "Address has already minted");
 
         // Mark the sender as having minted
         hasMinted[msg.sender] = true;
         mintedSum++;
-        uint256 newTokenId = totalSupply() + 1;
+        totalSupply++;
+        uint256 newTokenId = totalSupply;
         _safeMint(msg.sender, newTokenId);
         emit Minted(msg.sender, newTokenId);
+    }
+
+    function claimAirdrop(bytes32[] memory proof) public nonReentrant {
+        require(!mintingEnabled, "NFT sale in progress");
+        require(!airdropClaimed[msg.sender], "Already minted");
+        require(totalSupply + 1 <= MAX_SUPPLY, "Exceeds max supply");
+
+        bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+        require(MerkleProof.verify(proof, root, leaf), "Invalid Merkle Proof");
+        airdropClaimed[msg.sender] = true;
+        totalSupply++;
+        _safeMint(msg.sender, totalSupply);
     }
 
     // Withdraw function to allow owner to withdraw funds
@@ -73,9 +108,8 @@ contract GenesisBoomerNFT is ERC721Enumerable, ReentrancyGuard, Ownable {
         require(success, "Withdrawal failed");
     }
 
-    // User sign a message
+    // Genesis testing proof
     function signGenesisProof() public {
-        require(hasMinted[msg.sender], "Mint Genesis Boomer NFT first");
         emit GenesisBoomerProof(msg.sender, "Me Brave Boomer!");
     }
 }
---
+++ contracts/GenesisOAT.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GenesisOAT is ERC721, ReentrancyGuard, Ownable {
+    string public constant TOKEN_URI = "ipfs://QmTNZEjRoNn6AjQkiW5P8gC7RmNdd4GuwPntRS5hECTYPk";
+    bool public mintingEnabled;
+    uint256 public totalSupply;
+
+    // Mapping to keep track of addresses that have minted
+    mapping(address => bool) public hasMinted;
+
+    // Events
+    event MintingDisabled();
+    event MintingEnabled();
+    event Minted(address to, uint256 tokenId);
+
+    constructor() ERC721("GenesisOAT", "GOAT") Ownable(msg.sender) {}
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        _requireOwned(tokenId);
+        return _baseURI();
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return TOKEN_URI;
+    }
+
+    function enableMinting() public onlyOwner {
+        mintingEnabled = true;
+        emit MintingEnabled();
+    }
+
+    function disableMinting() public onlyOwner {
+        mintingEnabled = false;
+        emit MintingDisabled();
+    }
+
+    function mint() public nonReentrant {
+        require(mintingEnabled, "Minting is not enabled");
+        require(!hasMinted[msg.sender], "Address has already minted");
+
+        // Mark the sender as having minted
+        hasMinted[msg.sender] = true;
+        totalSupply++;
+        uint256 newTokenId = totalSupply;
+        _safeMint(msg.sender, newTokenId);
+
+        emit Minted(msg.sender, newTokenId);
+    }
+
+    // Withdraw function to allow owner to withdraw funds
+    function withdraw() public onlyOwner nonReentrant {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No funds to withdraw");
+        (bool success,) = owner().call{value: balance}("");
+        require(success, "Withdrawal failed");
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) public override {
+        revert("Transfer not allowed");
+    }
+}
